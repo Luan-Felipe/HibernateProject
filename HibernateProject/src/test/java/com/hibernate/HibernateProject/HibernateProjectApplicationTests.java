8787db649b277589f3675eba91add09a070a3a9c
Draeter <luanfel2009@gmail.com>
2020-11-16 12:36:52 -0300

    Ambientação de Projeto

---
+++ HibernateProject/src/test/java/com/hibernate/HibernateProject/HibernateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.HibernateProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
