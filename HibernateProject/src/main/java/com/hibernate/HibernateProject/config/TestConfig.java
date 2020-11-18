fb91c890594d5e035be7f36989451c5d9654b8d8
Draeter <luanfel2009@gmail.com>
2020-11-18 10:43:26 -0300

    Imlementação da classe Category

--- HibernateProject/src/main/java/com/hibernate/HibernateProject/config/TestConfig.java
+++ HibernateProject/src/main/java/com/hibernate/HibernateProject/config/TestConfig.java
@@ -1,7 +1,9 @@
 package com.hibernate.HibernateProject.config;
 
+import com.hibernate.HibernateProject.entities.Category;
 import com.hibernate.HibernateProject.entities.Order;
 import com.hibernate.HibernateProject.entities.User;
+import com.hibernate.HibernateProject.repositories.CategoryRepository;
 import com.hibernate.HibernateProject.repositories.OrderRepository;
 import com.hibernate.HibernateProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -32,8 +37,13 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2020-11-17T19:59:30Z"), u2);
         Order o3 = new Order(null, Instant.parse("2020-11-18T20:53:40Z"), u1);
 
+        Category cat1 = new Category(null, "Electronic");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 
 }
