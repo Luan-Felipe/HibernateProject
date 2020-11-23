6196ae76dd8617cdbd031f23e0724194bbb683c1
Draeter <luanfel2009@gmail.com>
2020-11-23 16:19:48 -0300

    Implementação de exceções personalizadas

--- HibernateProject/src/main/java/com/hibernate/HibernateProject/services/UserService.java
+++ HibernateProject/src/main/java/com/hibernate/HibernateProject/services/UserService.java
@@ -2,6 +2,7 @@ package com.hibernate.HibernateProject.services;
 
 import com.hibernate.HibernateProject.entities.User;
 import com.hibernate.HibernateProject.repositories.UserRepository;
+import com.hibernate.HibernateProject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Repository;
@@ -23,7 +24,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
