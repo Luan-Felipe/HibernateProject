a68ced99937ae0a44bede3e85c42306112db1c8b
Draeter <luanfel2009@gmail.com>
2020-11-23 15:43:08 -0300

    Implementação da função alterar dados cadastrais

--- HibernateProject/src/main/java/com/hibernate/HibernateProject/services/UserService.java
+++ HibernateProject/src/main/java/com/hibernate/HibernateProject/services/UserService.java
@@ -4,6 +4,7 @@ import com.hibernate.HibernateProject.entities.User;
 import com.hibernate.HibernateProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Id;
@@ -31,5 +32,17 @@ public class UserService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = repository.getOne(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
